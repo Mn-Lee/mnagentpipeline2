485212a6d4f5d43aa168575dd1c53dd202001cec
Pedmn <3404906301@qq.com>
2026-06-06 20:29:37 +0800

    bootstrap

---
+++ app/bootstrap/src/main/java/com/pedmn/mnagentpipeline2/MnAgentPipeline2Application.java
@@ -0,0 +1,13 @@
+package com.pedmn.mnagentpipeline2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MnAgentPipeline2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MnAgentPipeline2Application.class, args);
+    }
+
+}
